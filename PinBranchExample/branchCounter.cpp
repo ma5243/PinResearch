8e1c1500c3be6c3675a2f520ba38d90b99b0253a
ma5243 <abd07142011@gmail.com>
2021-08-24 15:05:56 -0400

    finished

--- PinBranchExample/branchCounter.cpp
+++ PinBranchExample/branchCounter.cpp
@@ -12,11 +12,18 @@ using std::string;
 ofstream OutFile;
 
 static UINT64 icount = 0;
+static UINT64 takenCount = 0;
 
 VOID docount() { icount++; }
+VOID countTaken() {takenCount++;}
 
 VOID Instruction(INS ins, VOID *v) {
-	INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount, IARG_END);
+	if(INS_IsBranch(ins) && INS_HasFallThrough(ins)) { //HasFallThrough checks if it is a conditional branch
+		INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount,IARG_BRANCH_TAKEN,IARG_END);//IARG_BRANCH_TAKEN sets a flag of whether the branch was taken or not
+	}
+	if(IARG_BRANCH_TAKEN){
+		countTaken();
+	}
 }
 
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
@@ -24,7 +31,9 @@ KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
 
 VOID Fini(INT32 code, VOID *v) {
     	OutFile.setf(ios::showbase);
-    	OutFile << "Count " << icount << endl;
+    	OutFile << "Total Branch Count " << icount << endl;
+	OutFile << "Branches Taken " << takenCount << endl;
+	OutFile << "Branches Not Taken " << icount - takenCount << endl;
     	OutFile.close();
 }
 
