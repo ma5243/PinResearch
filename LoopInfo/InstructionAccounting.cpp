34c10ac21d3f779d830a270738e83b7db8ed14c9
ma5243 <abd07142011@gmail.com>
2022-03-10 16:40:33 -0500

    Live in analysis changes

--- LoopInfo/InstructionAccounting.cpp
+++ LoopInfo/InstructionAccounting.cpp
@@ -5,62 +5,114 @@
 #include <unordered_map>
 #include <stack>
 #include <sstream>
+#include <set>
 
-#define START 0x400607
-#define END 0x400750
+#define START 0x400707
+#define END 0x400877
 
 std::ifstream instrFile;
 std::ofstream OutFile;
 
-class LoopStream {
-    public:      
-      UINT64 arithmeticIns;
-      UINT64 memoryReadIns;
-      UINT64 controlFlowIns; 
-      UINT64 memoryWriteIns;       
+class LoopStream
+{
+public:
+    UINT64 arithmeticIns;
+    UINT64 memoryReadIns;
+    UINT64 controlFlowIns;
+    UINT64 memoryWriteIns;
+    bool dependency;
 };
 
 std::vector<LoopStream> loopList;
 std::vector<std::string> addr;
+std::vector<INS> instr;
 
 int arithmetic_instr = 0;
 int mem_read_instr = 0;
 int mem_write_instr = 0;
 int control_flow_instr = 0;
 
-uint i=0;
+uint i = 0;
 uint addrr;
 
-VOID Application_Start(VOID *v) {
+VOID Application_Start(VOID *v)
+{
     instrFile.open("Instructions.txt");
     std::string myline;
-    while(instrFile) {
-        std::getline (instrFile,myline);
+    while (instrFile)
+    {
+        std::getline(instrFile, myline);
         addr.push_back(myline);
     }
     addr.pop_back();
 }
 
-//This method will keep track of all these instructions throughout and everytime a loop is detected
-//it will attach these statistic to the loop, and then reset the values. 
+// This method will keep track of all these instructions throughout and everytime a loop is detected
+// it will attach these statistic to the loop, and then reset the values.
 /*void doInstructionAccountingBBL(BBL bbl,vector<vector<int>>& vec) {
-	for( INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins) ) {
-		if(INS_IsMemoryRead(ins)) {
-			mem_instr++;
-		} else if(INS_IsCall(ins) || INS_IsBranch(ins)) {
-			control_flow_instr++;
-		} else if(INS_Opcode(ins) == XED_ICLASS_ADD || INS_Opcode(ins) == XED_ICLASS_SUB || 
-                    INS_Opcode(ins) == XED_ICLASS_AND || INS_Opcode(ins) == XED_ICLASS_IMUL || 
-                    INS_Opcode(ins) == XED_ICLASS_IDIV || INS_Opcode(ins) == XED_ICLASS_OR || 
-                    INS_Opcode(ins) == XED_ICLASS_XOR || INS_Opcode(ins) == XED_ICLASS_SHL || 
-                    INS_Opcode(ins) == XED_ICLASS_SHR || INS_Opcode(ins) == XED_ICLASS_NOT || 
-                    INS_Opcode(ins) == XED_ICLASS_NEG || INS_Opcode(ins) == XED_ICLASS_INC || 
+    for( INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins) ) {
+        if(INS_IsMemoryRead(ins)) {
+            mem_instr++;
+        } else if(INS_IsCall(ins) || INS_IsBranch(ins)) {
+            control_flow_instr++;
+        } else if(INS_Opcode(ins) == XED_ICLASS_ADD || INS_Opcode(ins) == XED_ICLASS_SUB ||
+                    INS_Opcode(ins) == XED_ICLASS_AND || INS_Opcode(ins) == XED_ICLASS_IMUL ||
+                    INS_Opcode(ins) == XED_ICLASS_IDIV || INS_Opcode(ins) == XED_ICLASS_OR ||
+                    INS_Opcode(ins) == XED_ICLASS_XOR || INS_Opcode(ins) == XED_ICLASS_SHL ||
+                    INS_Opcode(ins) == XED_ICLASS_SHR || INS_Opcode(ins) == XED_ICLASS_NOT ||
+                    INS_Opcode(ins) == XED_ICLASS_NEG || INS_Opcode(ins) == XED_ICLASS_INC ||
                     INS_Opcode(ins) == XED_ICLASS_DEC)  {
-			arithmetic_instr++;
-		}
-	}
+            arithmetic_instr++;
+        }
+    }
 }*/
 
+void doLiveInLiveOutAnalysis(LoopStream &elem)
+{
+    std::set<REG> liveIns;
+    std::set<REG> liveOuts;
+
+    int k = instr.size()-1;
+
+    for (; k >= 0; k--)
+    {
+        //std::cout << std::hex << INS_Address(instr.at(k)) << std::endl;
+        int numOperands = INS_OperandCount(instr.at(k));
+        for (int j = 0; j < numOperands; j++)
+        {
+            if (INS_OperandIsReg(instr.at(k), j))
+            {
+                REG rgstr = INS_OperandReg(instr.at(k), j);
+                if (INS_OperandReadAndWritten(instr.at(k), j))
+                { // corner case of a = a+b
+                    liveIns.insert(rgstr);
+                    liveOuts.insert(rgstr);
+                    std::cout << std::hex << INS_Address(instr.at(k)) << std::endl;
+                }
+                else if (INS_OperandReadOnly(instr.at(k), j))
+                {
+                    liveIns.insert(rgstr);
+                }
+                else if (INS_OperandWrittenOnly(instr.at(k), j))
+                {
+                    std::set<REG>::iterator iter = liveIns.find(rgstr);
+                    if(iter != liveIns.end()) {
+                        liveIns.erase(iter);
+                    } 
+                    liveOuts.insert(rgstr);
+                }
+            }
+        }
+    }
+    //std::cout << "end\n";
+    for(REG reg: liveIns) {
+        std::set<REG>::iterator iter = liveOuts.find(reg);
+        if(iter != liveOuts.end()) {
+            elem.dependency = true;
+            break;
+        }
+    }
+}
 
 INT32 Usage()
 {
@@ -70,25 +122,32 @@ INT32 Usage()
 
 VOID Trace(TRACE trace, VOID *v)
 {
-    //std::cout << myline << std::endl;
-    // Visit every basic block  in the trace
+    // std::cout << myline << std::endl;
+    //  Visit every basic block  in the trace
     for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
-    {   
-	    if(INS_Address(BBL_InsHead(bbl)) >= START && INS_Address(BBL_InsTail(bbl)) <= END) {
-            if(i==addr.size()) {
+    {
+        if (INS_Address(BBL_InsHead(bbl)) >= START && INS_Address(BBL_InsTail(bbl)) <= END)
+        {
+            if (i == addr.size())
+            {
                 break;
             }
-            //std::cout << addr.at(i) << std::endl;
-            if(addr.at(i) != "end") {
+            // std::cout << addr.at(i) << std::endl;
+            if (addr.at(i) != "end")
+            {
                 std::stringstream temp(addr.at(i));
                 temp >> addrr;
-            } else {
+            }
+            else
+            {
                 LoopStream elem;
                 elem.arithmeticIns = arithmetic_instr;
                 elem.controlFlowIns = control_flow_instr;
                 elem.memoryReadIns = mem_read_instr;
                 elem.memoryWriteIns = mem_write_instr;
+                elem.dependency = false;
 
+                doLiveInLiveOutAnalysis(elem);
                 loopList.push_back(elem);
                 i++;
 
@@ -97,81 +156,90 @@ VOID Trace(TRACE trace, VOID *v)
                 mem_read_instr = 0;
                 mem_write_instr = 0;
 
-                std::cout << "end" << std::endl;
             }
-            //std::cout << temp << std::endl;
-            if(addrr == INS_Address(BBL_InsTail(bbl))) {
+            // std::cout << temp << std::endl;
+            if (addrr == INS_Address(BBL_InsTail(bbl)))
+            {
 
-                //std::cout << INS_Address(BBL_InsHead(bbl)) << std::endl;
+                // std::cout << INS_Address(BBL_InsHead(bbl)) << std::endl;
 
-                for( INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins) ) {
-		            if(INS_IsMemoryRead(ins) || INS_Opcode(ins) == XED_ICLASS_POP) {
-                        std::cout << "Memory Read Instruction: " << std::hex << INS_Address(ins) << std::endl;
-			            mem_read_instr++;
-		            } else if(INS_IsMemoryWrite(ins) || INS_Opcode(ins) == XED_ICLASS_PUSH) {
-                        std::cout << "Memory Write Instruction: " << std::hex << INS_Address(ins) << std::endl;
-			            mem_write_instr++;
-                    } else if(INS_IsControlFlow(ins)) {
-                        std::cout << "Control Flow Instruction: " << std::hex << INS_Address(ins) << std::endl;
-			            control_flow_instr++;
-		            } else if(INS_Opcode(ins) == XED_ICLASS_ADD || INS_Opcode(ins) == XED_ICLASS_SUB || 
-                        INS_Opcode(ins) == XED_ICLASS_AND || INS_Opcode(ins) == XED_ICLASS_IMUL || 
-                        INS_Opcode(ins) == XED_ICLASS_IDIV || INS_Opcode(ins) == XED_ICLASS_OR || 
-                        INS_Opcode(ins) == XED_ICLASS_XOR || INS_Opcode(ins) == XED_ICLASS_SHL || 
-                        INS_Opcode(ins) == XED_ICLASS_SHR || INS_Opcode(ins) == XED_ICLASS_NOT || 
-                        INS_Opcode(ins) == XED_ICLASS_NEG || INS_Opcode(ins) == XED_ICLASS_INC || 
-                        INS_Opcode(ins) == XED_ICLASS_DEC || INS_Opcode(ins) == XED_ICLASS_MOV)  {
-                            std::cout << "Arithmetic Instruction: " << std::hex << INS_Address(ins) << std::endl;
-			                arithmetic_instr++;
-		            }
-	            }
+                for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins))
+                {
+                    if (INS_IsMemoryRead(ins) || INS_Opcode(ins) == XED_ICLASS_POP)
+                    {
+                        //std::cout << "Memory Read Instruction: " << std::hex << INS_Address(ins) << std::endl;
+                        mem_read_instr++;
+                    }
+                    else if (INS_IsMemoryWrite(ins) || INS_Opcode(ins) == XED_ICLASS_PUSH)
+                    {
+                        //std::cout << "Memory Write Instruction: " << std::hex << INS_Address(ins) << std::endl;
+                        mem_write_instr++;
+                    }
+                    else if (INS_IsControlFlow(ins))
+                    {
+                        //std::cout << "Control Flow Instruction: " << std::hex << INS_Address(ins) << std::endl;
+                        control_flow_instr++;
+                    }
+                    else if (INS_Opcode(ins) == XED_ICLASS_ADD || INS_Opcode(ins) == XED_ICLASS_SUB ||
+                             INS_Opcode(ins) == XED_ICLASS_AND || INS_Opcode(ins) == XED_ICLASS_IMUL ||
+                             INS_Opcode(ins) == XED_ICLASS_IDIV || INS_Opcode(ins) == XED_ICLASS_OR ||
+                             INS_Opcode(ins) == XED_ICLASS_XOR || INS_Opcode(ins) == XED_ICLASS_SHL ||
+                             INS_Opcode(ins) == XED_ICLASS_SHR || INS_Opcode(ins) == XED_ICLASS_NOT ||
+                             INS_Opcode(ins) == XED_ICLASS_NEG || INS_Opcode(ins) == XED_ICLASS_INC ||
+                             INS_Opcode(ins) == XED_ICLASS_DEC || INS_Opcode(ins) == XED_ICLASS_MOV)
+                    {
+                        //std::cout << "Arithmetic Instruction: " << std::hex << INS_Address(ins) << std::endl;
+                        arithmetic_instr++;
+                    }
+                    //std::cout << std::hex << INS_Address(ins) << std::endl;
+                    instr.push_back(ins);
+                }
                 i++;
-            }     
+            }
         }
     }
 }
 
-
 KNOB<std::string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
-    "o", "bblcount.out", "specify output file name");
+                                 "o", "bblcount.out", "specify output file name");
 
 // This function is called when the application exits
 VOID Fini(INT32 code, VOID *v)
 {
-   
-	OutFile << "Total number of Vectorizable Loops: " << loopList.size() << std::endl;
-	OutFile << "" << std::endl;
-    for(UINT32 i=0;i<loopList.size();i++) {
- 	    //UINT32 totalIters = loopList.at(i).iter + loopList.at(i).entries; 
-	    OutFile << "Total number of arithmetic instructions for loop " << i << ": "<< loopList.at(i).arithmeticIns << std::endl;
-	    OutFile << "Total number of read memory instructions for loop " << i << ": "<<  loopList.at(i).memoryReadIns << std::endl;
-	    OutFile << "Total number of write memory instructions for loop " << i << ": "<<  loopList.at(i).memoryWriteIns << std::endl;
-        OutFile << "Total number of control flow instructions for loop " << i << ": " << loopList.at(i).controlFlowIns  << std::endl;
-	    OutFile << "" << std::endl;
+
+    OutFile << "Total number of Vectorizable Loops: " << loopList.size() << std::endl;
+    OutFile << "" << std::endl;
+    for (UINT32 i = 0; i < loopList.size(); i++)
+    {
+        if(loopList.at(i).dependency == false) {
+            // UINT32 totalIters = loopList.at(i).iter + loopList.at(i).entries;
+            OutFile << "Total number of arithmetic instructions for loop " << i << ": " << loopList.at(i).arithmeticIns << std::endl;
+            OutFile << "Total number of read memory instructions for loop " << i << ": " << loopList.at(i).memoryReadIns << std::endl;
+            OutFile << "Total number of write memory instructions for loop " << i << ": " << loopList.at(i).memoryWriteIns << std::endl;
+            OutFile << "Total number of control flow instructions for loop " << i << ": " << loopList.at(i).controlFlowIns << std::endl;
+            OutFile << "" << std::endl;
+        }
     }
 }
 
-
-
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-    
-    PIN_InitSymbols();
-    if (PIN_Init(argc, argv)) return Usage();
 
-    PIN_AddApplicationStartFunction(Application_Start,0);
+    PIN_InitSymbols();
+    if (PIN_Init(argc, argv))
+        return Usage();
+
+    PIN_AddApplicationStartFunction(Application_Start, 0);
 
     OutFile.open(KnobOutputFile.Value().c_str());
 
     PIN_SetSyntaxIntel();
-    
+
     TRACE_AddInstrumentFunction(Trace, 0);
 
-    
     PIN_AddFiniFunction(Fini, 0);
-    
-    
+
     PIN_StartProgram();
-    
+
     return 0;
 }
--- LoopInfo/NewLoopDetectionMain.cpp
+++ LoopInfo/NewLoopDetectionMain.cpp
@@ -5,23 +5,23 @@
 #include <unordered_map>
 #include <stack>
 
-#define START 0x400607
-#define END 0x400750
+#define START 0x400707
+#define END 0x400877
 
 std::ofstream OutFile;
 std::ofstream bblTracker;
 
-class LoopStream {
-    public:
-      UINT64 iter = 0;      
-      UINT64 tailAddr;
-      UINT64 prevAddr=0;
-      UINT64 entries=0;
-      UINT64 numIns=0;
-    
-      std::vector<UINT64>bbls;          
-};
+class LoopStream
+{
+public:
+	UINT64 iter = 0;
+	UINT64 tailAddr;
+	UINT64 prevAddr = 0;
+	UINT64 entries = 0;
+	UINT64 numIns = 0;
 
+	std::vector<UINT64> bbls;
+};
 
 std::vector<UINT64> stack;
 
@@ -29,25 +29,29 @@ std::vector<UINT64> validBbls;
 std::vector<UINT64> validBblsIter;
 bool inLoop = false;
 
-//contains all loops that we found vectorizable
+// contains all loops that we found vectorizable
 std::vector<LoopStream> loopList;
 
 UINT64 curLoopIter = 0;
 UINT64 curLoopNumInsts = 0;
 UINT64 curLoopNumInstsPrev = curLoopNumInsts;
-//std::stack<BBL> basicBlocks;
+// std::stack<BBL> basicBlocks;
 
 INT32 Usage()
 {
-    std::cerr << "Usage: Not Implemented" << std::endl;
-    return -1;
+	std::cerr << "Usage: Not Implemented" << std::endl;
+	return -1;
 }
 
-VOID loopDetection(UINT64 tailAddr,UINT32 numIns) {
+VOID loopDetection(UINT64 tailAddr, UINT32 numIns)
+{
 	std::vector<UINT64>::iterator itr = std::find(stack.begin(), stack.end(), tailAddr);
-	if(itr != stack.end()) { //if already on the stack 
-		if(inLoop) { //If already in a loop and the bbl is on the stack, update statistics (this is the end of an iteration)
-			if(validBblsIter != validBbls) {
+	if (itr != stack.end())
+	{ // if already on the stack
+		if (inLoop)
+		{ // If already in a loop and the bbl is on the stack, update statistics (this is the end of an iteration)
+			if (validBblsIter != validBbls)
+			{
 				LoopStream elem;
 				elem.entries++;
 				elem.iter = curLoopIter;
@@ -59,24 +63,30 @@ VOID loopDetection(UINT64 tailAddr,UINT32 numIns) {
 				curLoopIter = 1;
 				validBbls = validBblsIter;
 
-				for(UINT32 i=0;i<validBbls.size();i++) {
+				for (UINT32 i = 0; i < validBbls.size(); i++)
+				{
 					bblTracker << validBbls.at(i) << "\n";
- 				}
+				}
 				bblTracker << "\n";
-			} else {
+			}
+			else
+			{
 				curLoopIter++;
 			}
-				curLoopNumInstsPrev = curLoopNumInsts;
-				curLoopNumInsts = numIns;
-				stack.clear();
-				stack.push_back(tailAddr);
-				validBblsIter.clear();
-				validBblsIter.push_back(tailAddr);
-		} else { //If not already in loop, that means we found a loop
-			curLoopIter=1;
+			curLoopNumInstsPrev = curLoopNumInsts;
+			curLoopNumInsts = numIns;
+			stack.clear();
+			stack.push_back(tailAddr);
+			validBblsIter.clear();
+			validBblsIter.push_back(tailAddr);
+		}
+		else
+		{ // If not already in loop, that means we found a loop
+			curLoopIter = 1;
 			inLoop = true;
-			UINT32 ind = std::distance(stack.begin(),itr);
-			while(ind < stack.size()) {
+			UINT32 ind = std::distance(stack.begin(), itr);
+			while (ind < stack.size())
+			{
 				validBbls.push_back(stack.at(ind));
 				ind++;
 			}
@@ -85,23 +95,32 @@ VOID loopDetection(UINT64 tailAddr,UINT32 numIns) {
 			stack.clear();
 			stack.push_back(tailAddr);
 		}
-	} else { //if not already on the stack
-		if(inLoop) { //if in a loop and current bbl not on the stack (either we're in the middle of the loop or the end)
+	}
+	else
+	{ // if not already on the stack
+		if (inLoop)
+		{ // if in a loop and current bbl not on the stack (either we're in the middle of the loop or the end)
 			validBblsIter.push_back(tailAddr);
 			std::vector<UINT64>::iterator itrTwo = std::find(validBbls.begin(), validBbls.end(), tailAddr);
-			if(itrTwo != validBbls.end()) { //if the current bbl is on the valid bbl list (we're in the middle of the loop)
+			if (itrTwo != validBbls.end())
+			{ // if the current bbl is on the valid bbl list (we're in the middle of the loop)
 				stack.push_back(tailAddr);
 				curLoopNumInsts += numIns;
-			} else { //if its not on valid bbl list, end of loop object
+			}
+			else
+			{ // if its not on valid bbl list, end of loop object
 				bool contains = false;
-				for(UINT64 j=0;j<loopList.size();j++) {
-					if(loopList.at(j).tailAddr == stack.at(stack.size()-1) && loopList.at(j).iter == curLoopIter && loopList.at(j).bbls == validBbls) {
-					loopList.at(j).entries++;
-					contains = true;
-					break;
+				for (UINT64 j = 0; j < loopList.size(); j++)
+				{
+					if (loopList.at(j).tailAddr == stack.at(stack.size() - 1) && loopList.at(j).iter == curLoopIter && loopList.at(j).bbls == validBbls)
+					{
+						loopList.at(j).entries++;
+						contains = true;
+						break;
 					}
 				}
-				if(!contains) {//create loop object on the end if doesn't already exist and set statistics
+				if (!contains)
+				{ // create loop object on the end if doesn't already exist and set statistics
 					LoopStream elem;
 					elem.entries++;
 					elem.iter = curLoopIter;
@@ -110,77 +129,76 @@ VOID loopDetection(UINT64 tailAddr,UINT32 numIns) {
 					elem.bbls = validBbls;
 					loopList.push_back(elem);
 
-					for(UINT32 i=0;i<validBbls.size();i++) {
+					for (UINT32 i = 0; i < validBbls.size(); i++)
+					{
 						bblTracker << validBbls.at(i) << std::endl;
- 					}
+					}
 					bblTracker << "end" << std::endl;
 				}
-				
+
 				curLoopNumInsts = 0;
 				inLoop = false;
 				validBbls.clear();
 				stack.clear();
-				stack.push_back(tailAddr);	
+				stack.push_back(tailAddr);
+			}
 		}
-		} else { //if not in a loop currently, this is just another bbl so push to stack	
+		else
+		{ // if not in a loop currently, this is just another bbl so push to stack
 			stack.push_back(tailAddr);
 		}
 	}
 }
 
-
-
 VOID Trace(TRACE trace, VOID *v)
 {
-    // Visit every basic block  in the trace
-    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
-    {
-		if(INS_Address(BBL_InsHead(bbl)) >= START && INS_Address(BBL_InsTail(bbl)) <= END) {
+	// Visit every basic block  in the trace
+	for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
+	{
+		if (INS_Address(BBL_InsHead(bbl)) >= START && INS_Address(BBL_InsTail(bbl)) <= END)
+		{
 			// Insert a call to docount before every bbl, passing the tail address
-        		BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)loopDetection, IARG_ADDRINT, INS_Address(BBL_InsTail(bbl)),IARG_UINT32, BBL_NumIns(bbl),IARG_END);
+			BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)loopDetection, IARG_ADDRINT, INS_Address(BBL_InsTail(bbl)), IARG_UINT32, BBL_NumIns(bbl), IARG_END);
 		}
-    }
+	}
 }
 
-
 KNOB<std::string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
-    "o", "bblcount.out", "specify output file name");
+								 "o", "bblcount.out", "specify output file name");
 
 // This function is called when the application exits
 VOID Fini(INT32 code, VOID *v)
-{   
+{
 	OutFile << "Total number of Vectorizable Loops: " << loopList.size() << std::endl;
 	OutFile << "" << std::endl;
-    for(UINT32 i=0;i<loopList.size();i++) {
-		OutFile << "Number of iterations for loop " << i << ": "<< loopList.at(i).iter << std::endl;
-		OutFile << "Total number of entries for loop " << i << ": "<<  loopList.at(i).entries << std::endl;
-		OutFile << "Instructions/iteration for loop " << i << ": " << loopList.at(i).numIns  << std::endl;
+	for (UINT32 i = 0; i < loopList.size(); i++)
+	{
+		OutFile << "Number of iterations for loop " << i << ": " << loopList.at(i).iter << std::endl;
+		OutFile << "Total number of entries for loop " << i << ": " << loopList.at(i).entries << std::endl;
+		OutFile << "Instructions/iteration for loop " << i << ": " << loopList.at(i).numIns << std::endl;
 		OutFile << "" << std::endl;
-    }
+	}
 }
 
-
-
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-    
-    PIN_InitSymbols();
-    if (PIN_Init(argc, argv)) return Usage();
 
-    OutFile.open(KnobOutputFile.Value().c_str());
+	PIN_InitSymbols();
+	if (PIN_Init(argc, argv))
+		return Usage();
+
+	OutFile.open(KnobOutputFile.Value().c_str());
 	bblTracker.open("Instructions.txt");
 
-    PIN_SetSyntaxIntel();
-    
-    TRACE_AddInstrumentFunction(Trace, 0);
+	PIN_SetSyntaxIntel();
 
-    
-    PIN_AddFiniFunction(Fini, 0);
-    
-    
-    PIN_StartProgram();
+	TRACE_AddInstrumentFunction(Trace, 0);
+
+	PIN_AddFiniFunction(Fini, 0);
+
+	PIN_StartProgram();
 
 	bblTracker.close();
-    
-    return 0;
+
+	return 0;
 }
