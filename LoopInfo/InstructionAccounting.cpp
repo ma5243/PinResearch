c7ff1d4e94d3ee90f0837017c13529aaa5fc1e04
ma5243 <abd07142011@gmail.com>
2022-03-21 13:07:11 -0400

    Debug changes

--- LoopInfo/InstructionAccounting.cpp
+++ LoopInfo/InstructionAccounting.cpp
@@ -20,20 +20,20 @@ public:
     UINT64 memoryReadIns;
     UINT64 controlFlowIns;
     UINT64 memoryWriteIns;
-    bool dependency;
+    BOOL dependency;
 };
 
-std::vector<LoopStream> loopList;
-std::vector<std::string> addr;
-std::vector<INS> instr;
+static std::vector<LoopStream> loopList;
+static std::vector<std::string> addr;
+static std::vector<INS> instr;
 
-int arithmetic_instr = 0;
-int mem_read_instr = 0;
-int mem_write_instr = 0;
-int control_flow_instr = 0;
+UINT64 arithmetic_instr = 0;
+UINT64 mem_read_instr = 0;
+UINT64 mem_write_instr = 0;
+UINT64 control_flow_instr = 0;
 
-uint i = 0;
-uint addrr;
+UINT64 i = 0;
+UINT64 addrr;
 
 VOID Application_Start(VOID *v)
 {
@@ -67,61 +67,66 @@ VOID Application_Start(VOID *v)
     }
 }*/
 
-
-bool containsInstruction(std::vector<INS> vec,INS in) {
-    for(uint j=0;j<vec.size();j++) {
-        if(INS_Address(vec.at(j)) == INS_Address(in)) {
+BOOL containsInstruction(std::vector<INS> vec, INS in)
+{
+    for (uint j = 0; j < vec.size(); j++)
+    {
+        if (INS_Address(vec.at(j)) == INS_Address(in))
+        {
             return true;
         }
     }
     return false;
 }
 
-void doLiveInLiveOutAnalysis(LoopStream &elem)
+VOID doLiveInLiveOutAnalysis(std::vector<INS> vec, LoopStream &elem)
 {
     std::set<REG> liveIns;
     std::set<REG> liveOuts;
 
-    int k = instr.size()-1;
-    //std::cout << k << std::endl;
+    int k = vec.size() - 1;
+    // std::cout << k << std::endl;
 
     for (; k >= 0; k--)
     {
-        std::cout << k << " " << std::hex << INS_Address(instr.at(k)) << std::endl;
-        int numOperands = INS_OperandCount(instr.at(k));
+        //std::cout << k << " " << std::hex << INS_Address(vec.at(k)) << std::endl;
+        int numOperands = INS_OperandCount(vec.at(k));
         for (int j = 0; j < numOperands; j++)
         {
-            if (INS_OperandIsReg(instr.at(k), j))
+            if (INS_OperandIsReg(vec.at(k), j))
             {
-                REG rgstr = INS_OperandReg(instr.at(k), j);
-                if (INS_OperandReadAndWritten(instr.at(k), j))
+                REG rgstr = INS_OperandReg(vec.at(k), j);
+                if (INS_OperandReadAndWritten(vec.at(k), j))
                 { // corner case of a = a+b
                     liveIns.insert(rgstr);
                     liveOuts.insert(rgstr);
-                    //std::cout << "Added to both: " << "Operand: " << k  << " Instruction: " << std::hex << INS_Address(instr.at(k)) << std::endl;
+                    // std::cout << "Added to both: " << "Operand: " << k  << " Instruction: " << std::hex << INS_Address(instr.at(k)) << std::endl;
                 }
-                else if (INS_OperandReadOnly(instr.at(k), j))
+                else if (INS_OperandReadOnly(vec.at(k), j))
                 {
                     liveIns.insert(rgstr);
-                    //std::cout << "Added to Live Ins: " << "Operand: " << k  << " Instruction: " << std::hex << INS_Address(instr.at(k)) << std::endl;
+                    // std::cout << "Added to Live Ins: " << "Operand: " << k  << " Instruction: " << std::hex << INS_Address(instr.at(k)) << std::endl;
                 }
-                else if (INS_OperandWrittenOnly(instr.at(k), j))
+                else if (INS_OperandWrittenOnly(vec.at(k), j))
                 {
                     std::set<REG>::iterator iter = liveIns.find(rgstr);
-                    if(iter != liveIns.end()) {
+                    if (iter != liveIns.end())
+                    {
                         liveIns.erase(iter);
-                        //std::cout << "Erased from Live Ins: " << "Operand: " << k  << " Instruction: " << std::hex << INS_Address(instr.at(k)) << std::endl;
-                    } 
+                        // std::cout << "Erased from Live Ins: " << "Operand: " << k  << " Instruction: " << std::hex << INS_Address(instr.at(k)) << std::endl;
+                    }
                     liveOuts.insert(rgstr);
-                    //std::cout << "Added to Live Outs: " << "Operand: " << k  << " Instruction: " << std::hex << INS_Address(instr.at(k)) << std::endl;
+                    // std::cout << "Added to Live Outs: " << "Operand: " << k  << " Instruction: " << std::hex << INS_Address(instr.at(k)) << std::endl;
                 }
             }
         }
     }
-    //std::cout << "end\n";
-    for(REG reg: liveIns) {
+    // std::cout << "end\n";
+    for (REG reg : liveIns)
+    {
         std::set<REG>::iterator iter = liveOuts.find(reg);
-        if(iter != liveOuts.end()) {
+        if (iter != liveOuts.end())
+        {
             elem.dependency = true;
             break;
         }
@@ -151,6 +156,58 @@ VOID Trace(TRACE trace, VOID *v)
             {
                 std::stringstream temp(addr.at(i));
                 temp >> addrr;
+
+                if (addrr == INS_Address(BBL_InsTail(bbl)))
+                {
+
+                    // std::cout << INS_Address(BBL_InsHead(bbl)) << std::endl;
+
+                    for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins))
+                    {
+                        if (INS_IsMemoryRead(ins) || INS_Opcode(ins) == XED_ICLASS_POP)
+                        {
+                            // std::cout << "Memory Read Instruction: " << std::hex << INS_Address(ins) << std::endl;
+                            mem_read_instr++;
+                        }
+                        else if (INS_IsMemoryWrite(ins) || INS_Opcode(ins) == XED_ICLASS_PUSH)
+                        {
+                            // std::cout << "Memory Write Instruction: " << std::hex << INS_Address(ins) << std::endl;
+                            mem_write_instr++;
+                        }
+                        else if (INS_IsControlFlow(ins))
+                        {
+                            // std::cout << "Control Flow Instruction: " << std::hex << INS_Address(ins) << std::endl;
+                            control_flow_instr++;
+                        }
+                        else if (INS_Opcode(ins) == XED_ICLASS_ADD || INS_Opcode(ins) == XED_ICLASS_SUB ||
+                                 INS_Opcode(ins) == XED_ICLASS_AND || INS_Opcode(ins) == XED_ICLASS_IMUL ||
+                                 INS_Opcode(ins) == XED_ICLASS_IDIV || INS_Opcode(ins) == XED_ICLASS_OR ||
+                                 INS_Opcode(ins) == XED_ICLASS_XOR || INS_Opcode(ins) == XED_ICLASS_SHL ||
+                                 INS_Opcode(ins) == XED_ICLASS_SHR || INS_Opcode(ins) == XED_ICLASS_NOT ||
+                                 INS_Opcode(ins) == XED_ICLASS_NEG || INS_Opcode(ins) == XED_ICLASS_INC ||
+                                 INS_Opcode(ins) == XED_ICLASS_DEC || INS_Opcode(ins) == XED_ICLASS_MOV)
+                        {
+                            // std::cout << "Arithmetic Instruction: " << std::hex << INS_Address(ins) << std::endl;
+                            arithmetic_instr++;
+                        }
+                        // std::cout << std::hex << INS_Address(ins) << std::endl;
+
+                        // if(std::find(instr.begin(), instr.end(), ins) == instr.end()) {
+                        // if(!containsInstruction(instr,ins)) {
+                        std::cout << std::hex << INS_Address(ins) << std::endl;
+                        instr.push_back(ins);
+
+                        std::cout << "Current size: " << instr.size() << std::endl;
+                        for (uint val = 0; val < instr.size(); val++)
+                        {
+                            std::cout << std::hex << INS_Address(instr.at(val)) << std::endl;
+                        }
+                        //}
+
+                        //}
+                    }
+                    i++;
+                }
             }
             else
             {
@@ -161,10 +218,14 @@ VOID Trace(TRACE trace, VOID *v)
                 elem.memoryWriteIns = mem_write_instr;
                 elem.dependency = false;
 
+                for (uint val = 0; val < instr.size(); val++)
+                {
+                    std::cout << std::hex << INS_Address(instr.at(val)) << std::endl;
+                }
                 std::cout << "end\n";
-                doLiveInLiveOutAnalysis(elem);
+                doLiveInLiveOutAnalysis(instr, elem);
                 instr.clear();
-                
+
                 loopList.push_back(elem);
                 i++;
 
@@ -172,54 +233,8 @@ VOID Trace(TRACE trace, VOID *v)
                 control_flow_instr = 0;
                 mem_read_instr = 0;
                 mem_write_instr = 0;
-
             }
             // std::cout << temp << std::endl;
-            if (addrr == INS_Address(BBL_InsTail(bbl)))
-            {
-
-                // std::cout << INS_Address(BBL_InsHead(bbl)) << std::endl;
-
-                for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins))
-                {
-                    if (INS_IsMemoryRead(ins) || INS_Opcode(ins) == XED_ICLASS_POP)
-                    {
-                        //std::cout << "Memory Read Instruction: " << std::hex << INS_Address(ins) << std::endl;
-                        mem_read_instr++;
-                    }
-                    else if (INS_IsMemoryWrite(ins) || INS_Opcode(ins) == XED_ICLASS_PUSH)
-                    {
-                        //std::cout << "Memory Write Instruction: " << std::hex << INS_Address(ins) << std::endl;
-                        mem_write_instr++;
-                    }
-                    else if (INS_IsControlFlow(ins))
-                    {
-                        //std::cout << "Control Flow Instruction: " << std::hex << INS_Address(ins) << std::endl;
-                        control_flow_instr++;
-                    }
-                    else if (INS_Opcode(ins) == XED_ICLASS_ADD || INS_Opcode(ins) == XED_ICLASS_SUB ||
-                             INS_Opcode(ins) == XED_ICLASS_AND || INS_Opcode(ins) == XED_ICLASS_IMUL ||
-                             INS_Opcode(ins) == XED_ICLASS_IDIV || INS_Opcode(ins) == XED_ICLASS_OR ||
-                             INS_Opcode(ins) == XED_ICLASS_XOR || INS_Opcode(ins) == XED_ICLASS_SHL ||
-                             INS_Opcode(ins) == XED_ICLASS_SHR || INS_Opcode(ins) == XED_ICLASS_NOT ||
-                             INS_Opcode(ins) == XED_ICLASS_NEG || INS_Opcode(ins) == XED_ICLASS_INC ||
-                             INS_Opcode(ins) == XED_ICLASS_DEC || INS_Opcode(ins) == XED_ICLASS_MOV)
-                    {
-                        //std::cout << "Arithmetic Instruction: " << std::hex << INS_Address(ins) << std::endl;
-                        arithmetic_instr++;
-                    }
-                    //std::cout << std::hex << INS_Address(ins) << std::endl;
-                    
-                    //if(std::find(instr.begin(), instr.end(), ins) == instr.end()) {
-                    if(!containsInstruction(instr,ins)) {
-                        std::cout << std::hex << INS_Address(ins) << std::endl;
-                        instr.push_back(ins);
-                    }    
-                   
-                    //}
-                }
-                i++;
-            }
         }
     }
 }
@@ -235,7 +250,8 @@ VOID Fini(INT32 code, VOID *v)
     OutFile << "" << std::endl;
     for (UINT32 i = 0; i < loopList.size(); i++)
     {
-        if(loopList.at(i).dependency == false) {
+        if (loopList.at(i).dependency == false)
+        {
             // UINT32 totalIters = loopList.at(i).iter + loopList.at(i).entries;
             OutFile << "Total number of arithmetic instructions for loop " << i << ": " << loopList.at(i).arithmeticIns << std::endl;
             OutFile << "Total number of read memory instructions for loop " << i << ": " << loopList.at(i).memoryReadIns << std::endl;
